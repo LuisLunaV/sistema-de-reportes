96b15a0219bdfd087c017a599e0a0238a7cd4262
luislunav <luis.a.luna.v@outlook.com>
2024-02-09 17:43:16 -0600

    Agregamos comentarios al codigo

--- middlewares/validate-properties.ts
+++ middlewares/validate-properties.ts
@@ -1,13 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult, ValidationError } from 'express-validator';
-
+// Middleware para validar propiedades en una solicitud
 const validateProperties=( req: Request, res: Response, next: NextFunction ):void | any =>{
+        // Validar la solicitud y obtener los errores de validación
     const errors = validationResult( req );
-
+  // Verificar si hay errores de validación
     if( !errors.isEmpty()){
+        // Si hay errores, enviar una respuesta de error con los detalles de los errores
         return res.status(400).json( errors.array() as  ValidationError[]);
     }
-
+ // Si no hay errores, continuar con el siguiente middleware en la cadena
     next();
 }
 
